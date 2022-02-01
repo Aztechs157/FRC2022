5b3926d4e6d4fb2d37d9a0f895ec8863b40bfc00
JamesBeeProg <JamesBeeProg@gmail.com>
2022-02-01 18:33:00 -0500

    Add docs to DoubleRange

--- src/main/java/frc/robot/lib/DoubleRange.java
+++ src/main/java/frc/robot/lib/DoubleRange.java
@@ -9,23 +9,50 @@ public class DoubleRange {
         this.high = high;
     }
 
+    /**
+     * Check if a value is contained within the range.
+     *
+     * @param value The value to check
+     * @return The result of the check
+     */
     public boolean contains(final double value) {
         return low <= value && value <= high;
     }
 
+    /**
+     * Get the length of the range.
+     *
+     * Specifically, the difference between the high and the low of the range. Also
+     * known as the "range" of the range.
+     *
+     * @return The length of the range
+     */
     public double length() {
         return high - low;
     }
 
-    /*
-     * scale(inputRange, value, outputRange)
+    /**
+     * Clamp a value to a number within this range.
      *
-     * inputRange.scale(value, outputRange)
+     * Specifically, any numbers lower than `this.low` will result in `this.low`,
+     * any numbers higher than `this.high` will result in `this.high`, and anything
+     * in between isn't modified.
+     *
+     * @param value The value to clamp
+     * @return The clamped value
+     */
+    public double applyClamp(final double value) {
+        return Math.max(low, Math.min(value, high));
+    }
+
+    /*
+     * Scale a number within `inputRange` to the corresponding number within
+     * `outputRange`.
+     *
+     * Example: Let input range be 0 to 2, and the output range be 0 to 10. The
+     * input 1 will result in an output 5, the input 2 will result in 10, etc.
      */
     public static double scale(final DoubleRange inputRange, final double inputValue, final DoubleRange outputRange) {
-        // return (((value - inputRange.high) * outputRange.length()) /
-        // inputRange.length()) + outputRange.high;
-
         // scale
         var scale = outputRange.length() / inputRange.length();
 
