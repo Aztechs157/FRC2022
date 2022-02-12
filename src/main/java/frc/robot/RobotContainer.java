789bcc9ab4a1d1a10fec33a5396e4d89dafe6d5b
Eric-157 <eedwards445566@gmail.com>
2022-02-12 15:02:20 -0500

    changed a comment because it was buggin me

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -28,12 +28,10 @@ import frc.robot.drive.TeleopDrive;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
- * Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in
- * the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of
- * the robot (including
- * subsystems, commands, and button mappings) should be declared here.
+ * Command-based is a "declarative" paradigm, very little robot logic should
+ * actually be handled in the {@link Robot} periodic methods (other than the
+ * scheduler calls). Instead, the structure of the robot (including subsystems,
+ * commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
     private final DriverController driverController = new DriverController();
