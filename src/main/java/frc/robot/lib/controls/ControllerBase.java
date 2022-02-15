7d16f63b2c4beb9c6eee95316703c2904ea3536e
JamesBeeProg <JamesBeeProg@gmail.com>
2022-02-15 15:05:06 -0500

    Revert "Make ControllerBase#getAxis and getButton return suppliers"
    This reverts commit 81a989ebde856c3d2984d19fdad4ad871a02aa44.

--- src/main/java/frc/robot/lib/controls/ControllerBase.java
+++ src/main/java/frc/robot/lib/controls/ControllerBase.java
@@ -27,17 +27,27 @@ public class ControllerBase<ButtonKey, AxisKey> implements Sendable {
      * @return A {@link ButtonInput} and {@link Button} representing the input
      */
     public Button button(final ButtonKey buttonKey) {
-        return new Button(getSelectedLayout().getButton(buttonKey));
+        return new Button(() -> getButton(buttonKey));
     }
 
     /**
-     * Get a axis from the currently selected layout.
+     * Get a button from the currently selected layout
+     *
+     * @param buttonKey Which button to retrieve
+     * @return The boolean representing the input
+     */
+    public boolean getButton(final ButtonKey buttonKey) {
+        return getSelectedLayout().getButton(buttonKey).getAsBoolean();
+    }
+
+    /**
+     * Get a axis from the currently selected layout
      *
      * @param axisKey Which axis to retrieve
      * @return A {@link AxisInput} representing the input
      */
-    public AxisInput axis(final AxisKey axisKey) {
-        return getSelectedLayout().getAxis(axisKey);
+    public double getAxis(final AxisKey axisKey) {
+        return getSelectedLayout().getAxis(axisKey).getAsDouble();
     }
 
     /**
