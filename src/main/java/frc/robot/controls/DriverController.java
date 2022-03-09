e08eb1f9a83aa9e64cdb69e508fac4743f92af9b
JamesBeeProg <JamesBeeProg@gmail.com>
2022-03-09 18:12:28 -0500

    Reenable driving

--- src/main/java/frc/robot/controls/DriverController.java
+++ src/main/java/frc/robot/controls/DriverController.java
@@ -10,12 +10,12 @@ public class DriverController extends ControllerBase<ButtonKey, AxisKey> {
         final var defaultLayout = new Layout("Default");
         final var logitech = new LogitechGamepadF310(0);
 
-        // defaultLayout.assign(AxisKey.DriveSpeedX,
-        // logitech.leftStickX.scaled(DRIVE_INPUT_SCALE));
-        // defaultLayout.assign(AxisKey.DriveSpeedY,
-        // logitech.leftStickY.scaled(DRIVE_INPUT_SCALE).inverted());
-        // defaultLayout.assign(AxisKey.DriveRotation,
-        // logitech.rightStickX.scaled(DRIVE_INPUT_SCALE));
+        defaultLayout.assign(AxisKey.DriveSpeedX,
+                logitech.leftStickX.scaled(DRIVE_INPUT_SCALE));
+        defaultLayout.assign(AxisKey.DriveSpeedY,
+                logitech.leftStickY.scaled(DRIVE_INPUT_SCALE).inverted());
+        defaultLayout.assign(AxisKey.DriveRotation,
+                logitech.rightStickX.scaled(DRIVE_INPUT_SCALE));
 
         defaultLayout.assign(AxisKey.DriveSpeedX, () -> 0);
         defaultLayout.assign(AxisKey.DriveSpeedY, () -> 0);
