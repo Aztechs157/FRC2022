8322355a0d2c8f63dc02e4e121681c3cc3cfaabd
JamesBeeProg <jamesbeeprog@gmail.com>
2022-03-20 19:22:28 -0400

    Remove LayoutToggler

--- src/main/java/frc/robot/input/OperatorInputs.java
+++ src/main/java/frc/robot/input/OperatorInputs.java
@@ -1,7 +1,6 @@
 package frc.robot.input;
 
 import frc.robot.lib.input.Layout;
-import frc.robot.lib.input.LayoutToggler;
 import frc.robot.lib.input.ShuffleLayoutChooser;
 import frc.robot.lib.input.models.LogitechGamepadF310;
 
@@ -11,17 +10,6 @@ public class OperatorInputs extends ShuffleLayoutChooser {
         final var defaultLayout = add(new Layout("Default"));
         final var logitech = new LogitechGamepadF310(0);
 
-        defaultLayout.assign(Keys.Button.Hello, logitech.a);
-
-        final var printFooLayout = new Layout("Print Foo");
-        printFooLayout.assign(Keys.DebugButton.PrintFoo, logitech.a);
-        final var printBarLayout = new Layout("Print Bar");
-        printBarLayout.assign(Keys.DebugButton.PrintBar, logitech.b);
-
-        final var printLayouts = new LayoutToggler(printFooLayout, printBarLayout);
-        defaultLayout.assign(Keys.DebugButton.PrintFoo, printLayouts.button(Keys.DebugButton.PrintFoo));
-        defaultLayout.assign(Keys.DebugButton.PrintBar, printLayouts.button(Keys.DebugButton.PrintBar));
-
-        logitech.start.whenPressed(printLayouts::toggle);
+        defaultLayout.assign(Keys.DebugButton.PrintBar, logitech.a);
     }
 }
